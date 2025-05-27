b88e2b71b4a696b95f0a408a8cd912198e5ed0f6
surujmiah6896 <surujmiah6896@gmail.com>
2025-05-27 12:59:37 +0600

    get pending task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -39,12 +39,26 @@ class TaskController extends Controller
             $task = Task::findOrFail($id);
             $task->is_completed = $validated['is_completed'];
             $task->save();
-            
+
             return sendResponseWithData('task', $task, true, 'Update Successfully', 200);
 
         }catch (\Throwable $th) {
             return sendResponseWithMessage(false, $th->getMessage(), 500);
         }
 
+    }
+
+
+    // get pending tasks
+    public function getPending()
+    {
+        try {
+            $tasks = Task::where('is_completed', false)->get();
+            return sendResponseWithData('tasks', $tasks, true, 'Post retrieved successfully', 200);
+        } catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+            return sendResponseWithMessage(false, 'Post not found', 404);
+        } catch (\Throwable $th) {
+            return sendResponseWithMessage(false, $th->getMessage(), 500);
+        }
     }
 }

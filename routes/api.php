45cd0657a3f5c0b08f71b0791cc8943cc5db2dd6
surujmiah6896 <surujmiah6896@gmail.com>
2025-05-27 12:42:39 +0600

    user validation and create

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::apiResource('posts', PostController::class);
+Route::post('/register', [AuthController::class, 'register']);

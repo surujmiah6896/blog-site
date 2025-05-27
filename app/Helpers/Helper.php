f17a71f59b7d706cec16e928cd50f5fe0fb10814
surujmiah6896 <surujmiah6896@gmail.com>
2025-05-27 12:15:20 +0600

    post validation and store

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!function_exists('sendResponseWithMessage')){
+    function sendResponseWithMessage($status = false, $message = null, $code = 200){
+        return response()->json([
+            'status' => $status,
+            'message' => $message
+        ], $code);
+
+    }
+}
+
+if(!function_exists('sendResponseWithData')){
+    function sendResponseWithData($dataKey =null, $data = null, $status = false, $message = null, $code = 200){
+        return response()->json([
+            'status' => $status,
+            'message' => $message,
+            $dataKey => $data ?? [],
+        ], $code = 200);
+    }
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    // Create a post
+    public function store(Request $request)
+    {
+        try{
+            $data = $request->validate([
+                'title' => 'required|string|max:255',
+                'content' => 'required|string',
+            ]);
+
+            $data['created_at'] = now();
+
+            $post = Post::create($data);
+
+            return sendResponseWithData('post', $post, true, 'Create Successfully', 201);
+        }catch(\Throwable $th){
+            return sendResponseWithMessage(false, $th->getMessage(), 500);
+        }
+
+    }
+
+    
 }

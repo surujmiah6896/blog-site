101abbeb03447837ac1e75c9780fe0d31e0f97b3
surujmiah6896 <surujmiah6896@gmail.com>
2025-05-27 12:52:48 +0600

    validation and store task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // add a Task
+    public function store(Request $request)
+    {
+        try{
+            $validated = $request->validate([
+                'title' => 'required|string',
+            ]);
+
+            $task = Task::create([
+                'title' => $validated['title'],
+                'is_completed' => false,
+                'created_at' => now(),
+            ]);
+            return sendResponseWithData('task', $task, true, 'Create Successfully', 201);
+        }catch(\Throwable $th){
+            return sendResponseWithMessage(false, $th->getMessage(), 500);
+        }
+
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,15 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TaskController;
 
+//route for post
 Route::apiResource('posts', PostController::class);
+
+//route for register
 Route::post('/register', [AuthController::class, 'register']);
+
+//route for task
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{id}', [TaskController::class, 'update']);
+Route::get('/tasks/pending', [TaskController::class, 'getPending']);
